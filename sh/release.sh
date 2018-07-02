12d1ad02f4214a90cb31e54af4fccfff1b96e34f
fenghou <luyi_shinian@sina.cn>
2018-07-02 11:14:24 +0800

    :meno::meno:

---
+++ sh/release.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+read -p "Releasing project - are you sure? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Releasing..."
+
+
+  # commit build
+  git add -A
+  echo "Enter Commit message: "
+  read COMMIT
+  git commit -m "$COMMIT"
+
+  # publish
+  echo "publishing git..."
+  git push origin master
+
+
+fi
