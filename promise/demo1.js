e82684252ad51042aaf1a0ec42982a0294fde295
wangjiafeng <wangjiafeng@youzan.com>
2018-12-26 21:54:35 +0800

    [feature]: new demo

--- promise/demo1.js
+++ promise/demo1.js
@@ -1,11 +1,35 @@
-const awaitLoad = ()=> new Promise((reslove)=>{
-    var arr = [1,2,3,4,5]
+const awaitLoad1 = ()=> new Promise((reslove)=>{
+    var arr = [1,2]
+    console.log('--------');
+    setTimeout(()=>{
+      reslove(arr)
+    },3000)
+})
+const awaitLoad2 = ()=> new Promise((reslove)=>{
+  var arr = [3,4]
+  console.log('==========');
+  setTimeout(()=>{
     reslove(arr)
+  },3000)
+})
+const awaitLoad3 = ()=> new Promise((reslove)=>{
+  var arr = [4,5]
+  console.log('~~~~~~~~~~~');
+  setTimeout(()=>{
+    reslove(arr)
+  },2000)
 })
 
 const start = async ()=>{
-	const aaa = awaitLoad();
-	console.log(aaa);
+  console.log('start A1')
+	const a1 = awaitLoad1();
+	console.log('start A2')
+	const a2 = awaitLoad2();
+	await awaitLoad3();
+	const aa1 = await a1;
+	const aa2 = await a2;
+  console.log(aa1);
+	console.log(aa2);
 }
 
-start()
+start();
---
+++ study/jiahuo.js
@@ -0,0 +1,14 @@
+// var a = {
+//   b: 10
+// }
+// var c = 2;
+// c += a.c || 480;
+// console.log(c);
+//
+// var d = a.b === 11 ? 1 : a.c || 480
+// console.log(d);
+//
+// var ccc = ''
+// console.log(!+ccc);
+
+
