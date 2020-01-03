677121b28e1ada4fd3b81e707f270b62c378c4c4
wangjiafeng <wangjiafeng@youzan.com>
2020-01-03 10:37:19 +0800

    feat: demo

--- leetCode/strongPasswordChecker.js
+++ leetCode/strongPasswordChecker.js
@@ -8,103 +8,38 @@
  * 插入、删除、替换任一字符都算作一次修改。
  * @param s
  */
-const rules = [
-  s => {
-    let check = false;
-    let path = 0;
-    if (s.length >=6 && s.length <=20) {
-      check = true;
-    } else {
-      path = Math.min(Math.abs(s.length - 6), Math.abs(s.length - 20));
-    }
-    return {
-      check,
-      path,
-      dp: path,
-      priority: 20,
-      type: s.length < 6 ? 'add' : s.length > 20 ? 'del' : '',
-    }
-  },
-  s => {
-    const check = /\d{1}/.test(s);
-    return {
-      check,
-      path: Number(!check),
-      dp: Number(!check),
-      priority: 10,
-      type: 'add',
-    }
-  },
-  s => {
-    const check = /[A-Z]{1}/.test(s);
-    return {
-      check,
-      path: Number(!check),
-      dp: Number(!check),
-      priority: 10,
-      type: 'add',
-    }
-  },
-  s => {
-    const check = /[a-z]{1}/.test(s);
-    return {
-      check,
-      path: Number(!check),
-      dp: Number(!check),
-      priority: 10,
-      type: 'add',
-    }
-  },
-  s => {
-    const check = s.match(/(.)\1{1,}/g) || [];
-    const path = check.reduce((path, item) => {
-      if (item.length > 2) {
-        let _path = Math.floor(item.length/3);
-        path += _path;
-      }
-      return path;
-    } , 0);
-    return {
-      check: path === 0,
-      path,
-      dp: path,
-      priority: 30,
-      type: 'all',
-    }
-  },
-]
 
+// 不包含 小写字母 1
+// 不包含 大写字母 1
+// 不包含 一个数字 1
+// a
+// < 6 + b
+// > 20 - c
+
+// aaa b 加
+// aaa c 减
+//
+/**
+ * @param {string} s
+ * @return {number}
+ */
 var strongPasswordChecker = function(s) {
-  const result = rules.reduce((_c, rule) => {
-    let params = rule(s);
-    _c.check = params.check && _c.check;
-    if (!params.check) _c.list.push(params);
-    return _c;
-  }, { check: true, list: []});
+  let ar_count = 0; // 需要添加 或者 可以通过替换解决
+  if (!/[a-z]/.test(s)) ar_count++;
+  if (!/[A-Z]/.test(s)) ar_count++;
+  if (!/[0-9]/.test(s)) ar_count++;
+  let a_count = s.length >= 6 ? 0 : 6 - s.length; // 需要通过添加解决
+  let d_count = s.length <= 20 ? 0 : s.length - 20; // 需要通过删除解决
 
-  if (result.check) return 0;
-  const list = result.list.sort((a, b)=> a.priority - b.priority);
-  let map = {};
-  list.forEach(item => {
-    if (!map[item.priority]) {
-      map[item.priority] = item.path;
-    } else {
-      map[item.priority] += item.path;
-    }
+  console.log(ar_count, a_count, d_count)
 
-  });
-  let path = 0;
-  console.log(map);
-  Object.keys(map).forEach(item => {
-    path = Math.max(map[item], path)
-  })
-  console.log(path);
-  return path;
+
 };
 
 // strongPasswordChecker('1010101010aaaB10101010'); // 2
-strongPasswordChecker('ABABABABABABABABABAB1'); // 2
+// strongPasswordChecker('ABABABABABABABABABAB1'); // 2
 // strongPasswordChecker('abababababababababaaa'); // 3
 // strongPasswordChecker('aaaaaaaaaaaaaaaaaaaaa'); // 7
 // strongPasswordChecker('1111111111'); // 3
-strongPasswordChecker(''); // 6
+// strongPasswordChecker(''); // 6
+strongPasswordChecker('aaaaaaaaaaaaaaaaaaaaa')
